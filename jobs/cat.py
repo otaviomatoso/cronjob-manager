0c94d90dcf4b19a83de81400549ca526baaf596b
omatoso <omatoso@convoso.com>
2023-01-17 23:30:17 -0300

    create cat job

---
+++ jobs/cat.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+class Cat:
+
+    @staticmethod
+    def write_to_file_job():
+        with open("cat_file.txt", "a") as file:
+            file.write("cat\n")
+            print(f"Wrote 'cat' to file at {datetime.datetime.now()}")
