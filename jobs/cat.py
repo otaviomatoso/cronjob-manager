a936efa0172ee98eeda92488094e3460ba5f14e2
omatoso <omatoso@convoso.com>
2023-01-17 23:33:11 -0300

    import only datetime function, and not the whole package

--- jobs/cat.py
+++ jobs/cat.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 
 class Cat:
@@ -7,4 +7,4 @@ class Cat:
     def write_to_file_job():
         with open("cat_file.txt", "a") as file:
             file.write("cat\n")
-            print(f"Wrote 'cat' to file at {datetime.datetime.now()}")
+            print(f"Wrote 'cat' to file at {datetime.now()}")
