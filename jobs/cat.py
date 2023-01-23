51ca5ae9e53bf3527c4c71797900ff74d20fd9a5
otaviomatoso <omatoso@convoso.com>
2023-01-22 21:01:05 -0300

    remove cat class

--- jobs/cat.py
+++ jobs/cat.py
@@ -1,10 +1,7 @@
 from datetime import datetime
 
 
-class Cat:
-
-    @staticmethod
-    def write_to_file_job():
-        with open("cat_file.txt", "a") as file:
-            file.write("cat\n")
-            print(f"Wrote 'cat' to file at {datetime.now()}")
+def write_cat_to_file():
+    with open("cat_file.txt", "a") as file:
+        file.write("cat\n")
+        print(f"Wrote 'cat' to file at {datetime.now()}")
