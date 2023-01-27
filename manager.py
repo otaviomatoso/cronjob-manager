68d775c98840ad2b2fd1d7d241a7c7111147892c
otaviomatoso <omatoso@convoso.com>
2023-01-26 21:16:33 -0300

    create a package for cat_manager for allowing import it

--- manager.py
+++
@@ -1,8 +0,0 @@
-from jobs import write_cat_to_file
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-
-scheduler = BlockingScheduler()
-if __name__ == '__main__':
-    scheduler.add_job(func=write_cat_to_file, trigger='interval', seconds=60)
-    scheduler.start()
