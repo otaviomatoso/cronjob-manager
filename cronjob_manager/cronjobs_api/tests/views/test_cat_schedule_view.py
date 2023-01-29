c09ab1b23a0ed9427a9217979d5be0de3127a9d2
otaviomatoso <omatoso@convoso.com>
2023-01-29 15:39:42 -0300

    rename test function; use reverse function

--- cronjob_manager/cronjobs_api/tests/views/test_cat_schedule_view.py
+++ cronjob_manager/cronjobs_api/tests/views/test_cat_schedule_view.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from django.test import Client
+from django.urls import reverse
 import time
-import os
 
 
 class TestCatScheduleView(TestCase):
@@ -9,13 +9,13 @@ class TestCatScheduleView(TestCase):
     def setUp(self) -> None:
         self.client = Client()
 
-    def test_cat_schedule_given_valid_request_and_2_seconds_delay_when_post_is_called_then_response_is_200_and_file_is_created_and_contains_cat(self):
+    def test_given_valid_2_seconds_when_post_then_200_and_file_is_created_and_contains_cat(self):
         # Arrange
         delay = 2
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..', 'cat_file.txt')
+        file_path = 'cat_file.txt'
 
         # Act
-        response = self.client.post('/api/cat_schedule/', data={'delay': delay})
+        response = self.client.post(reverse('cronjobs_api:cat_schedule'), data={'delay': delay})
 
         # Assert
         self.assertEqual(response.status_code, 200)
