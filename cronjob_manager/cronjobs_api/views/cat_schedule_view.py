b17130b57f2f83bd9d7e0f614ce7cdf32daf0986
otaviomatoso <omatoso@convoso.com>
2023-03-22 19:26:06 -0300

    update cat_schedule_view

--- cronjob_manager/cronjobs_api/views/cat_schedule_view.py
+++ cronjob_manager/cronjobs_api/views/cat_schedule_view.py
@@ -1,15 +1,24 @@
 from cat_manager.jobs import write_cat_to_file
+from cat_manager.manager import scheduler
 from datetime import datetime, timedelta
 from django.http import JsonResponse
 from django.views import View
-from cat_manager.manager import scheduler
+from enums import JobTypes
+from http import HTTPStatus
+import pytz
 
 
 class CatScheduleView(View):
 
+    UTC_TIMEZONE = pytz.utc
+
     def post(self, request, *args, **kwargs):
         delay = int(request.GET.get('delay', 5))
-        next_run_date = datetime.now() + timedelta(seconds=delay)
-        scheduler.add_job(func=write_cat_to_file, trigger='date', run_date=next_run_date)
-        next_run_date = datetime.strftime(next_run_date, '%H:%M:%S')
-        return JsonResponse({'message': f'Job will run at {next_run_date}'})
+        run_date = datetime.now(pytz.utc) + timedelta(seconds=delay)
+        scheduler.add_job(func=write_cat_to_file, trigger='date', run_date=run_date,
+                          kwargs={'key': JobTypes.CAT, 'file_name': 'cat_file.txt'},
+                          misfire_grace_time=None)
+
+        run_date_pst = run_date.astimezone(tz=pytz.timezone('US/Pacific')).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        print(f'[API] Will write cat to file at {run_date_pst}')
+        return JsonResponse({'message': f'Job will run at {run_date_pst}'}, status=HTTPStatus.CREATED)
