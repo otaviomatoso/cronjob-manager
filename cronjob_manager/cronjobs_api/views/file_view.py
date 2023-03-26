3863e763b7e824f7e5a9a29c58d3dc1d56eedfc3
otaviomatoso <omatoso@convoso.com>
2023-03-26 11:56:20 -0300

    add check when getting pk; improving responses

--- cronjob_manager/cronjobs_api/views/file_view.py
+++ cronjob_manager/cronjobs_api/views/file_view.py
@@ -7,14 +7,16 @@ from http import HTTPStatus
 class FileView(View):
 
     def get(self, request, *args, **kwargs):
+        if not kwargs.get('pk'):
+            return JsonResponse({'message': 'Missing file id'}, status=HTTPStatus.BAD_REQUEST)
+
         if file := File.objects.filter(id=kwargs.get('pk')).first():
             file_dict = file.to_dict()
-            return JsonResponse(file_dict, status=HTTPStatus.OK)
-
-        return JsonResponse({'message': 'No file found with the given id'}, status=HTTPStatus.NOT_FOUND)
+            return JsonResponse({'file': file_dict}, status=HTTPStatus.OK)
+        return JsonResponse({'message': 'No file found with the given id', 'file': None}, status=HTTPStatus.OK)
 
     def delete(self, request, *args, **kwargs):
         if File.objects.filter(id=(file_id := kwargs.get('pk'))).delete()[0] > 0:
             return JsonResponse({'message': f'File with id {file_id} deleted'}, status=HTTPStatus.NO_CONTENT)
 
-        return JsonResponse({'message': 'File not found with the given id'}, status=HTTPStatus.NOT_FOUND)
+        return JsonResponse({'message': 'No file found with the given id'}, status=HTTPStatus.NOT_FOUND)
